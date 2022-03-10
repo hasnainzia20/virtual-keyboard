4e81bb03cf41d5dc9ed1120f481d2dbefd2cf300
hasnainzia20 <hasnainzia438@gmail.com>
2022-03-10 18:24:25 +0500

    commit3

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 const button = document.querySelector('#btn');
 var textarea = document.getElementById("textarea");
 var keyboard  = document.getElementById("keyboard");
+var keyBoardBtn = document.querySelector('.keyBtn'); 
+var capslock = document.getElementById("btn-21");
+
 var linebreak = '\n';
-
-
 var key = [
     "1", "2","3","4","5","6","7","8","9","0","Backspace",
     "q","w","e","r","t","y","u","i","o","p",
-    "capslock","a","s","d","f","g","h","i","j","k","l","enter",
+    "a","s","d","f","g","h","i","j","k","l","enter",
     "z","x","c","v","b","n","m",",",".","?","/",
     " "
 ]
+// var key2 = key.map(x => x.toUpperCase());
 
-// const insertLineBreak = ["backspace", "p", "enter", "?"].indexOf(key) !== -1;
+// console.log(key2);
 
 key.forEach(myFunction);
+
+
 
 function myFunction(item,index) {
     var char = document.createElement("button");
@@ -26,6 +30,7 @@ function myFunction(item,index) {
     char.setAttribute('id','btn-' + index);
     char.textContent += item;
     // console.log(char);
+
     keyboard.appendChild(char);
     if(item == "Backspace" || item=="p" || item=="enter" || item=="/" ) {
         keyboard.appendChild(lineBreak);
@@ -34,9 +39,9 @@ function myFunction(item,index) {
         // e.preventDefault();
         // char.classList.toggle("uCase");
     }
-    else if (item == " ") {
-        document.getElementById("btn-44").style.padding = "15px 130px";
-    }
+    // else if (item == " ") {
+    //     document.getElementById("btn-44").style.padding = "15px 130px";
+    // }
 
     char.addEventListener('click', (e) => {
         if(item == "Backspace") {
@@ -47,10 +52,9 @@ function myFunction(item,index) {
         }
         else if (item == "capslock") {
             e.preventDefault();
-            // toUpperCaseLetter();
-            var keyBoardBtn = document.querySelector('.keyBtn'); 
-            keyBoardBtn.forEach(toUpperCaseLetter());
-        }
+            // key.splice(0, key.length, ...key2);
+            // key2.forEach(myFunction);
+        }    
         else if (item == " ") {
             textarea.value +=  " ";
         }
@@ -62,11 +66,55 @@ function myFunction(item,index) {
     
 }
 
+// key2.forEach(myFunction2);
 
-function toUpperCaseLetter() {
-    keyBoardBtn.classList.add("uCase");
-    // keyBoardBtn.classList.add("uCase");
-    console.log(keyBoardBtn);
-}
+// function myFunction2(item,index) {
+//     var char = document.createElement("button");
+//     var lineBreak = document.createElement("br");
+//     var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    
+//     char.style.backgroundColor = "#" + randomColor;
+//     char.setAttribute('class','keyBtn');
+//     char.setAttribute('id','btn-' + index);
+//     char.textContent += item;
+//     // console.log(char);
 
+//     keyboard.appendChild(char);
+//     if(item == "BACKSPACE" || item=="P" || item=="ENTER" || item=="/" ) {
+//         keyboard.appendChild(lineBreak);
+//     }
+//     else if (item == "CAPSLOCK") {
+//         // e.preventDefault();
+//         // char.classList.toggle("uCase");
+//     }
+//     else if (item == " ") {
+//         document.getElementById("btn-44").style.padding = "15px 130px";
+//     }
 
+//     char.addEventListener('click', (e) => {
+//         if(item == "BACKSPACE") {
+//         textarea.value = textarea.value.slice(0,-1);
+//         }
+//         else if(item == "ENTER"){
+//             textarea.value = textarea.value + "\n";
+//         }
+//         else if (item == "CAPSLOCK") {
+//             e.preventDefault();
+//             key.forEach(myFunction);
+//         }    
+//         else if (item == " ") {
+//             textarea.value +=  " ";
+//         }
+//         else{
+//         textarea.value += item;
+//         document.getElementById("view-wrapper").style.backgroundColor = "#" + randomColor;
+//     }
+//     })
+    
+// }
+
+// capslock.addEventListener('click', (e) => {
+    
+//     key2.forEach(myFunction);
+
+// });
